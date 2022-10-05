1be88e62c61013c0bf557058f7b358d3ede28e4b
Blazej Gruszka <blazej.gruszka@displate.com>
2022-10-05 15:46:57 +0200

    add optional params can be send to New Relic

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 version = '1.0.9'
 
 setup(
-    name = 'supervisor_newrelic',
+    name = 'supervisor_newrelic_displate',
     version = version,
     description = 'Collection of Supervisor plugins to provide metrics and monitoring within New Relic',
-    author = 'Sportlobster',
-    author_email = 'info@sportlobster.com',
+    author = 'Displate',
+    author_email = '',
     license = 'MIT',
-    url = 'https://github.com/sportlobster/supervisor-newrelic',
-    download_url = 'https://github.com/sportlobster/supervisor-newrelic/tarball/%s' % version,
+    url = 'https://github.com/displate/supervisor-newrelic',
+    download_url = 'https://github.com/displate/supervisor-newrelic/tarball/%s' % version,
     long_description = open('README.rst').read(),
     keywords = ['supervisor', 'supervisord', 'newrelic', 'monitoring'],
     classifiers = [],
@@ -23,7 +23,7 @@ setup(
     tests_require = ['nose', 'mock'],
     test_suite = 'nose.collector',
 
-    packages = ['supervisor_newrelic'],
+    packages = ['supervisor_newrelic_displate'],
     entry_points = {
         'console_scripts': {
             'supervisor_newrelic_status = supervisor_newrelic.status:main',
--- supervisor_newrelic/status.py
+++ supervisor_newrelic/status.py
@@ -17,9 +17,10 @@ from supervisor import childutils
 
 class Status(object):
 
-    def __init__(self, account, key):
+    def __init__(self, account, key, additional_params):
         self.account = account
         self.key = key
+        self.additional_params = additional_params
         self.stdin = sys.stdin
         self.stdout = sys.stdout
 
@@ -38,7 +39,7 @@ class Status(object):
             pheaders, pdata = childutils.eventdata(payload+'\n')
 
             if self.send(pheaders['processname'], pheaders['groupname'],
-                    headers['eventname']):
+                         headers['eventname'], self.additional_params):
                 childutils.listener.ok(self.stdout)
             else:
                 childutils.listener.fail(self.stdout)
@@ -47,13 +48,17 @@ class Status(object):
                 break
 
 
-    def send(self, name, group, status):
-        data = [{
+    def send(self, name, group, status, additional_args):
+        event = {
                 'eventType': 'Supervisor:Status',
                 'processName': name,
                 'groupName': group,
                 'status': status,
-        }]
+        }
+
+        event.update(additional_args)
+
+        data = [event]
         url = ('https://insights-collector.newrelic.com/'
                 'v1/accounts/%s/events' % self.account)
         headers = {'X-Insert-Key': self.key}
@@ -65,9 +70,12 @@ def main():
     parser = argparse.ArgumentParser(description='Supervisor event listener')
     parser.add_argument('--account', '-a', help='New Relic account number')
     parser.add_argument('--key', '-k', help='New Relic Insights insert key')
+    parser.add_argument('-p', '--param', action='append', nargs=2, metavar=('key', 'value'), help='Additional param which will be send to New Relic')
     args = parser.parse_args()
 
-    status = Status(args.account, args.key)
+    additional_params = {arg[0]: arg[1] for arg in args.param} if args.param is not None else {}
+
+    status = Status(args.account, args.key, additional_params)
     status.run()
 
 if __name__ == '__main__':
