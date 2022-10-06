c57c09590752f26f935ec2bfb2533fc0fefe6932
Blazej Gruszka <blazej.gruszka@displate.com>
2022-10-06 13:11:10 +0200

    add optional params can be send to New Relic

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
-version = '1.0.9'
+version = '1.1.0'
 
 setup(
-    name = 'supervisor_newrelic_displate',
+    name = 'supervisor_newrelic',
     version = version,
     description = 'Collection of Supervisor plugins to provide metrics and monitoring within New Relic',
-    author = 'Displate',
-    author_email = '',
+    author = 'Sportlobster',
+    author_email = 'info@sportlobster.com',
     license = 'MIT',
-    url = 'https://github.com/displate/supervisor-newrelic',
-    download_url = 'https://github.com/displate/supervisor-newrelic/tarball/%s' % version,
+    url = 'https://github.com/sportlobster/supervisor-newrelic',
+    download_url = 'https://github.com/sportlobster/supervisor-newrelic/tarball/%s' % version,
     long_description = open('README.rst').read(),
     keywords = ['supervisor', 'supervisord', 'newrelic', 'monitoring'],
     classifiers = [],
@@ -23,7 +23,7 @@ setup(
     tests_require = ['nose', 'mock'],
     test_suite = 'nose.collector',
 
-    packages = ['supervisor_newrelic_displate'],
+    packages = ['supervisor_newrelic'],
     entry_points = {
         'console_scripts': {
             'supervisor_newrelic_status = supervisor_newrelic.status:main',
